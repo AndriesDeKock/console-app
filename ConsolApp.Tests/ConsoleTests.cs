cc4e5107a47fa1e8b0580d80fadd0a26e67b05e4
AndriesDeKock <andries@snowblu.com>
2019-05-01 17:45:54 +0200

    Add project files.

---
+++ ConsolApp.Tests/ConsoleTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp;
+using Xunit;
+
+namespace ConsolApp.Tests
+{
+    public class ConsoleTests
+    {
+        [Theory]
+        [InlineData(5, 4, 1)]
+        [InlineData(5, 9, -4)]
+        [InlineData(5, 1.3, 3.7)]
+        [InlineData(double.MaxValue, 5, double.MaxValue)]
+        public void Subtract_SimpleValueShouldCalculate(double x, double y, double expected) {
+
+            // Arrange
+
+            // Act
+            double actual = ConsoleValidator.Subtract(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class ConsoleValidator
+    {
+        static void Main(string[] args)
+        {
+            //expected result should always be value of 5
+            double expected = 5;
+            //request user to enter value less than 5
+            Console.WriteLine("Please enter a number less than 5");
+            //validate user input
+            if (!double.TryParse(Console.ReadLine(), out double input))
+            {
+                //invalid numerical value
+                Console.WriteLine("Invalid number value received");
+            }
+            else
+            {
+                //determine what value is required to get to expected value of 5
+                Console.WriteLine(string.Concat("The number required to get to 5 is ",  Subtract(expected, input)));
+            }
+
+            Console.Read();
+
+        }
+
+        public static double Subtract(double x, double y) {
+
+            return (x - y);
+
+        }
+    }
+}
